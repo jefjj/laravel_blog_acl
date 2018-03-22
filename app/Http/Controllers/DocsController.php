6e2c584872bdfdef25f842af56da38d5b0b7d2ae
Jeferson Costa <jj.jeferson@gmail.com>
2018-03-22 15:56:21 -0300

    Prototipo de uso do PhpWord criado

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -3,18 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\Style\Font;
+use PhpOffice\PhpWord\IOFactory;
 
 class DocsController extends Controller
 {
     public function index (Request $request)
     {
+        $doc = $request->session()->get('doc');
+
         return view('docs.word-doc')->with([
             'info' => 'Office Word Document PHP Lib',
+            'doc' => $doc,
         ]);
     }
 
     function save (Request $request) 
     {
-        return redirect()->route('docGet');
+        // Creating the new document...
+        $phpWord = new PhpWord();
+        
+        // Creating a title style
+        $titleStyle = new Font();
+        $titleStyle->setBold(true);
+        $titleStyle->setName('Tahoma');
+        $titleStyle->setSize(18);
+        $titleStyle->setColor('#999999');
+
+        // Adding an empty Section to the document...
+        $section = $phpWord->addSection();
+        
+        // Adding Text element to the Section having font styled by default...
+        $section->addText($request->input('titulo'), $titleStyle);
+        $section->addText($request->input('texto'));
+        $section->getStyle()->setColsNum($request->input('cols'));
+
+        // Saving the document as OOXML file...
+        $objWriter = IOFactory::createWriter($phpWord, 'Word2007');
+        $objWriter->save('docs\documento-word.docx');
+
+        $request->session()->flash('doc', url('/docs/documento-word.docx'));      
+
+        return redirect()->back();
     }
 }
--- resources/views/docs/word-doc.blade.php
+++ resources/views/docs/word-doc.blade.php
@@ -1,4 +1,5 @@
-@extends('layouts.app') @section('content')
+@extends('layouts.app') 
+@section('content')
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
@@ -8,19 +9,29 @@
     <hr>
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2">
+            @if ($doc)
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>Arquivo gerado!</strong> Clique para baixar: <a href="{{ $doc }}">{{ $doc }}</a>
+                </div>
+            @endif
             <div class="well">
                 <form method="post" action="{{ Route('docPost') }}" enctype="application/x-www-form-urlencoded">
                     <fieldset>
-                    <legend>{{ $info }}</legend>
+                        <legend>{{ $info }}</legend>
                         {{ csrf_field() }}
                         <div class="form-group">
                             <label for="photo">Título</label>
-                            <input type="text" id="titulo" name="titulo" class="form-control">
+                            <input type="text" id="titulo" name="titulo" class="form-control" required>
                         </div>
                         <div class="form-group">
-                          <label for="photo">Título</label>
-                          <textarea name="texto" id="texto" rows="4" class="form-control"></textarea>
-                      </div>
+                            <label for="photo">Texto</label>
+                            <textarea name="texto" id="texto" rows="4" class="form-control" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="photo">Qtd. de colunas</label>
+                            <input type="number" name="cols" id="cols" class="form-control" value="1" required></textarea>
+                        </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary">Enviar</button>
                         </div>
